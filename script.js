7abe7bd4e830841c9ad011a1902faa1103d725d6
KeerthanaViyas <79353338+KeerthanaViyas@users.noreply.github.com>
2021-02-22 13:53:27 +0530

    console output

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-jQuery(function (){
+$(document).ready(function(){
     $(window).scroll(function(){
+        console.log("into scroll function");
         if(this.scrollY > 20){
+            console.log("add sticky");
             $('.navbar').addClass("sticky");
         }
         else{
+            console.log("remove sticky");
             $('.navbar').removeClass("sticky");
         }
     })
